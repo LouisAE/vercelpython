a284a467188113c3094fd84c16c40bec8b974d6d
LouiszAE <fzr4422@126.com>
2022-09-17 09:25:07 +0000

    test path

--- api/index.py
+++ api/index.py
@@ -1,6 +1,1 @@
-import sys
-import os
-CURRENT_DIR = os.path.split(os.path.abspath(__file__))[0]  # 当前目录
-config_path = CURRENT_DIR.rsplit('/', 1)[0]  # 上三级目录
-sys.path.append(config_path)
 import main
