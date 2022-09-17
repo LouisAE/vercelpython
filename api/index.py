fd34b5f60ec8861411f068df2bf15a51d34c52e1
LouiszAE <fzr4422@126.com>
2022-09-17 09:18:36 +0000

    change path and executable metho

--- api/index.py
+++ api/index.py
@@ -1,11 +1,6 @@
-from http.server import BaseHTTPRequestHandler
- 
- 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write("test python api".encode())
-        return
+import sys
+import os
+CURRENT_DIR = os.path.split(os.path.abspath(__file__))[0]  # 当前目录
+config_path = CURRENT_DIR.rsplit('/', 1)[0]  # 上三级目录
+sys.path.append(config_path)
+import main
